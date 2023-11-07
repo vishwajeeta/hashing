6567f38bb77c73dd33b2fd0de44478304d598744
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-11-07 21:03:53 +0530

    setup versel config

--- hashing/settings.py
+++ hashing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c3#lb=2$$gg+j!ngp^t@%4lo@67=umwcyd%!=*h5d$w0(ym2_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
--- hashing/wsgi.py
+++ hashing/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hashing.settings')
 
 application = get_wsgi_application()
+
+#VERCEL
+app=application
