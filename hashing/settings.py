b0b6ba495c06582eb076c9ee9eec7d6d6c843100
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-11-08 07:17:29 +0530

    trying

--- hashing/settings.py
+++ hashing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c3#lb=2$$gg+j!ngp^t@%4lo@67=umwcyd%!=*h5d$w0(ym2_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'hashing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
@@ -115,11 +110,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+
+
